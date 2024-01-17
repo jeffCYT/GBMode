dd9d3a025956e3d45ca3dee9bd8520e0a21135a6
Jeff CYT <jeff@icada.com>
2024-01-17 10:13:33 +0800

    skeleton on connection.ts

--- src/connection.ts
+++ src/connection.ts
@@ -5,3 +5,32 @@
  */
 
 import * as vscode from "vscode";
+
+// var status = Disconnected | Connecting | Connected;
+
+function stop() {
+    // return destroy();
+}
+
+
+function sendRequest(request) {
+    /** case status
+     * Disconnected => start(); sendRequest()
+     * Connecting => push(request) into queue?
+     * Connected => LSPClient.onReady(); LSPClient.sendRequest(request)
+     */
+}
+
+function onNotification() {
+}
+
+function onError() {
+}
+
+function start() {
+    /** case status
+     * Connected => OK() from LSP
+     * Connecting => keep promise()
+     * Disconnected => LSPClient.make();
+     */
+}
